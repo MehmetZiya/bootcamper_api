92b7baa1a1669b78f67397bb6a2dfc01f918047e
Mehmet Ziya <mehmetziya82@gmail.com>
2022-01-30 21:45:05 +0100

    added bootcamps routes and controllers files

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const { cyan } = require('colors')
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI)
+    console.log(
+      `MongoDB Connected: ${connection.connection.host}`.cyan.underline
+    )
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+//MiddleWare Files
+// const logger = require('./middleware/logger')
+
+//Route Files
+const bootcamps = require('./routes/bootcamps')
 
 dotenv.config({ path: './config/config.env' })
 
+//Connect Database
+connectDB()
+
 const app = express()
+
+//Middlewares
+// app.use(logger) custom logger middleware
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+  app.use(morgan('dev'))
+}
+
+//Routes
+app.use('/api/v1/bootcamps', bootcamps)
+
+app.get('/', (req, res) => {
+  res.send('Hello from express')
+})
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta
+      .bold.italic
+  )
 )
