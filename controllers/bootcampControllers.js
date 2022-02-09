1755694db3bbe5fd1ecb1b3f96bd1a699dac8d2d
Mehmet Ziya <mehmetziya82@gmail.com>
2022-02-09 11:37:28 +0100

    added pagination functionality

--- controllers/bootcampControllers.js
+++ controllers/bootcampControllers.js
@@ -9,15 +9,61 @@ const geocoder = require('../utils/geocoder')
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query
-  let queryStr = JSON.stringify(req.query)
+  const reqQuery = { ...req.query }
+  const removeField = ['select', 'sort', 'page', 'limit']
+  removeField.forEach((param) => delete reqQuery[param])
+  let queryStr = JSON.stringify(reqQuery)
+
+  //create operators like ( $gt, $lte etc) MongoDB documentation
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
 
-  console.log(queryStr)
   query = Bootcamp.find(JSON.parse(queryStr))
+
+  //Select fields return only selected data
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ')
+    query = query.select(fields)
+  }
+
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ')
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt')
+  }
+
+  //Pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 25
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+  query = query.skip(startIndex).limit(limit)
+
   const bootcamps = await query
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps })
+
+  //Pagination result
+  const pagination = {}
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    }
+  }
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  })
 })
 
 // @desc Get single bootcamps
