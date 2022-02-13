8b21da74a6a9c699abf8e763555171138e876db4
Mehmet Ziya <mehmetziya82@gmail.com>
2022-02-13 17:42:21 +0100

    added API security packages and API documentation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const express = require('express')
 const {
   register,
   login,
+  logout,
   getMe,
   forgotPassword,
   resetPassword,
@@ -15,6 +16,7 @@ const { protect } = require('../middleware/auth')
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/logout', logout)
 router.get('/me', protect, getMe)
 router.put('/updateuser', protect, updateNameAndEmail)
 router.put('/updatepassword', protect, updatePassword)
